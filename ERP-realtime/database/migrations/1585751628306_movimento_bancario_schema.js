d564d9ae8a41bf2fd42ef4ab460d99e2bb811960
rafael duarte amaral <rafael.duarte.amaral@gmail.com>
2020-04-11 23:14:48 -0300

    correção de erro no mimento bancario

--- ERP-realtime/database/migrations/1585751628306_movimento_bancario_schema.js
+++ ERP-realtime/database/migrations/1585751628306_movimento_bancario_schema.js
@@ -4,32 +4,33 @@
 const Schema = use('Schema')
 
 class MovimentoBancarioSchema extends Schema {
-  up () {
-    this.create('movimento_bancarios', (table) => {
-      table.increments()
-      table.string('banco_debito', 40)
-      table.string('banco_credito', 30)
-      table.integer('valor').unsigned()
-      table.date('data_lancamento')
-      table.string('descricao', 150)
-      table.integer('empresa_id').unsigned()
-      table
-      .foreign('empresa_id')
-      .references('id')
-      .inTable('empresas')
-      .onDelete('cascade')
-      table
-      .foreign('bancos_id')
-      .references('id')
-      .inTable('bancos')
-      .onDelete('cascade')
-      table.timestamps()
-    })
-  }
+    up() {
+        this.create('movimento_bancarios', (table) => {
+            table.increments()
+            table.string('banco_debito', 40)
+            table.string('banco_credito', 30)
+            table.integer('valor').unsigned()
+            table.date('data_lancamento')
+            table.string('descricao', 150)
+            table.integer('empresa_id').unsigned()
+            table
+                .foreign('empresa_id')
+                .references('id')
+                .inTable('empresas')
+                .onDelete('cascade')
+            table.integer('bancos_id').unsigned();
+            table
+                .foreign('bancos_id')
+                .references('id')
+                .inTable('bancos')
+                .onDelete('cascade')
+            table.timestamps()
+        })
+    }
 
-  down () {
-    this.drop('movimento_bancarios')
-  }
+    down() {
+        this.drop('movimento_bancarios')
+    }
 }
 
 module.exports = MovimentoBancarioSchema
